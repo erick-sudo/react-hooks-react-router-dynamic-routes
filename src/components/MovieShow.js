9e2741eb0ddeb325c602d023dee9127775738ebd
Erick Ochieng <erickochieng766@gmail.com>
2023-01-19 17:20:29 +0300

    Movie Show Component

---
+++ src/components/MovieShow.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MovieShow() {
+    return (
+        <div>
+            <h1>Movie Show Component</h1>
+        </div>
+    )
+}
+
+export default MovieShow;
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MoviesShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+
+  const match = useRouteMatch();
+  console.log(match);
   return (
     <div>
       <MoviesList movies={movies} />
+
+      <Route>
+        <MoviesShow />
+      </Route>
     </div>
   );
 }
+
 export default MoviesPage;
